58f81acae55fae27f370f31bfc1218ca86ad61bb
mlo-olagunju <oladele@skillupafrica.com.ng>
2023-11-29 00:47:51 +0100

    Made it mobile responsive

---
+++ main.js
@@ -0,0 +1,13 @@
+const showNavItem = document.getElementById('showNavItem')
+
+function showNav (){
+    if (showNavItem.style.display === 'none'){
+        showNavItem.style.display = 'block'
+        // themenu.style.display = 'none'
+    } 
+    else {
+        showNavItem.style.display = 'none'
+        // theX.style.display = 
+    }
+
+}
