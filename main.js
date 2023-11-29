bfa5fcbb067a70b16b803a891fd28796ea0d3545
mlo-olagunju <oladele@skillupafrica.com.ng>
2023-11-30 00:57:59 +0100

    added a toggle hamburger responsive and fully functional nav bar  using javascript

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
-const showNavItem = document.getElementById('showNavItem')
+const hamburbuger = document.querySelector('.hamburger');
+const NavAside = document.querySelector('.nav-aside');
 
-function showNav (){
-    if (showNavItem.style.display === 'none'){
-        showNavItem.style.display = 'block'
-        // themenu.style.display = 'none'
-    } 
-    else {
-        showNavItem.style.display = 'none'
-        // theX.style.display = 
-    }
+hamburbuger.addEventListener('click', () => {
+    hamburbuger.classList.toggle('active');
+    NavAside.classList.toggle('active');
+})
 
-}
+document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburbuger.classList.remove("active");
+    NavAside.classList.remove("active");
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const showNavItem = document.getElementById('showNavItem')
+
+// function showNav (){
+//     if (showNavItem.style.display === 'none'){
+//         showNavItem.style.display = 'block'
+//         // themenu.style.display = 'none'
+//     } 
+//     else {
+//         showNavItem.style.display = 'none'
+//         // theX.style.display = 
+//     }
+
+// }
